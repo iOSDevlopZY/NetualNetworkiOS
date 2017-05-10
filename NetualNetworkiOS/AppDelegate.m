def5b946ba91d0e6454e02c733eb4a34ddecc510
iOSDevlopZY <Developer_Yi@developer-YideMacBook-Pro.local>
2017-05-10 10:20:06 +0800

    修改

--- NetualNetworkiOS/AppDelegate.m
+++ NetualNetworkiOS/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TouchID.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+     [TouchID create];
     return YES;
 }
 
@@ -40,6 +42,7 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   
 }
 
 
